e29c35eeed7dd381a93fb9078628930656e108cc
rtadlapure-loyaltymethods <rtadlapure@loyaltymethods.com>
2023-04-04 00:33:04 +0530

    typo changes

--- index.js
+++ index.js
@@ -9,36 +9,33 @@ setInterval(()=>{
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
     let AM_PM = "AM";
-    
-    
-    // let milliSec = Date.now();
 
+    //converting time into 12 hours format
     if(hours > 12){
         hours = hours - 12;
         AM_PM = "PM";
-
     }
+
+    //adding zero before single digit number
     if(hours < 10){
         hours = "0" + hours;
     }
+
     if(minutes < 10){
         minutes = "0" + minutes;
     }
+
     if(seconds < 10){
         seconds = "0" + seconds;
     }
+
     ISOtime.textContent = hours + ":" + minutes + ":" + seconds + " " + AM_PM;
     dateAndUTC.textContent = date.toString().slice(0,33);
 
-    // epochInMilliSec.textContent = "Current Epoch Time in MilliSec : " + Math.floor(date.getTime()/1000);
-    
     let secSinceEpoch = function secSinceEpoch(){
         return Math.floor(Date.now()/1000) || 0
     }
 
     epochInMilliSec.textContent = "Current Epoch Time in MilliSec : " + secSinceEpoch();
-
-    // console.log(date.now()-date);
-    
 });
 
